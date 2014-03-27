324142b7712848f37ea06368f4857fa9a49493b4
micpp <micheleportopires@gmail.com>
2014-03-27 10:57:40 -0300

    Update lz5.cpp
    Nova tentativa, alterações realizadas: adição de 1 comentário na última linha!!!

--- lz5.cpp
+++ lz5.cpp
@@ -1,23 +1,24 @@
 #include<iostream>
 
 int main(){
-  std::cout<<"Olá, eu sou o LZ-5 e vou te ajudar a simular uma população para estudos em Melhoramento Genético Animal!"<<std::endl;
+  std::cout<<"Olï¿½, eu sou o LZ-5 e vou te ajudar a simular uma populaï¿½ï¿½o para estudos em Melhoramento Genï¿½tico Animal!"<<std::endl;
   
-  std::cout<<"Você gostaria de continuar?"<<std::endl;
-  std::cout<<"Sim[s] ou Não[n]"<<std::endl;
+  std::cout<<"Vocï¿½ gostaria de continuar?"<<std::endl;
+  std::cout<<"Sim[s] ou Nï¿½o[n]"<<std::endl;
 
   char resp='s';
   std::cin>>resp;
 
   switch(resp){
   case 'S':
-  case 's': std::cout<<"Infelizmente não será possível pois o Rafael e a Michele ainda não programaram nada e estou esperando para trabalhar. Volte mais tarde. TCHAU!!!!"<<std::endl;
+  case 's': std::cout<<"Infelizmente nï¿½o serï¿½ possï¿½vel pois o Rafael e a Michele ainda nï¿½o programaram nada e estou esperando para trabalhar. Volte mais tarde. TCHAU!!!!"<<std::endl;
     break;
   case 'N':
-  case 'n': std::cout<<"Que bom, eu não teria nada para fazer mesmo! A Michele e o Rafael ainda não enviaram nada para mim. Volte outra hora. TCHAU!!!"<<std::endl;
+  case 'n': std::cout<<"Que bom, eu nï¿½o teria nada para fazer mesmo! A Michele e o Rafael ainda nï¿½o enviaram nada para mim. Volte outra hora. TCHAU!!!"<<std::endl;
     break;  
-  default: std::cout<<"Respondeu mal!!! Não vou mais trabalhar então! TCHAU!!!" <<std::endl;
+  default: std::cout<<"Respondeu mal!!! Nï¿½o vou mais trabalhar entï¿½o! TCHAU!!!" <<std::endl;
   }
 
   return 0;
 }
+//Nova tentativa de pull req.
