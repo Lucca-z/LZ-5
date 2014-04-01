0e80b748266663d0d4ff45e33b98aa49b62b14a3
micpp <micheleportopires@gmail.com>
2014-04-01 15:05:59 -0300

    Update alelle.cpp

--- alelle.cpp
+++ alelle.cpp
@@ -9,13 +9,10 @@ Alelle::Alelle():id(count++){
   state = 0;
   cout<<"Oi sou o construtor 1"<<endl;
 }
-
+/*
 void Alelle::setAlelle(short int st){
   cout<<"Editando o estado do alelle com o valor "<<st<<endl;  
   state = st;
   cout<<"Edição concluída"<<endl;
 }
-
-void Alelle::getAlelle() const{
-  cout<<" O estado do alelle "<<id<<" é: "<<state<<endl;
-}
+*/
